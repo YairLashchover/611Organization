054aa8c47c8161de721d6fc751843398872881b8
mugo2 <yairninja321@gmail.com>
2026-07-28 17:24:56 +0300

    שינויי התחברות

--- app.js
+++ app.js
@@ -191,7 +191,7 @@ app.get("/logout", (req, res) => {
 // ============================
 
 app.listen(PORT, "0.0.0.0", () => {
-    console.log("Server running on port 10.70.248.190:3000");
+    console.log("Server running on port http://10.70.248.190:3000");
 });
 
 // testing phone connection
